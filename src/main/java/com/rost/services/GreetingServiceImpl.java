760b40f1915c7399e7e906231c0e8e323d27e792
Rost <coderost@yandex.ru>
2023-01-28 17:33:38 +0300

    Refactor service via Lombok.

--- src/main/java/com/rost/services/GreetingServiceImpl.java
+++ src/main/java/com/rost/services/GreetingServiceImpl.java
@@ -4,7 +4,9 @@ import com.rost.grpc.GreetingServiceGrpc;
 import com.rost.grpc.GreetingServiceOuterClass.HelloRequest;
 import com.rost.grpc.GreetingServiceOuterClass.HelloResponse;
 import io.grpc.stub.StreamObserver;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(staticName = "instance")
 public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
     @Override
     public void greeting(HelloRequest req, StreamObserver<HelloResponse> respObserver) {
