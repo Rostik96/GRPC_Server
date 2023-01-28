5cd8624921585d1e59d50da176f80a0df5c27772
Rost <coderost@yandex.ru>
2023-01-28 17:34:57 +0300

    Create server into App main-method.

--- src/main/java/com/rost/App.java
+++ src/main/java/com/rost/App.java
@@ -1,11 +1,23 @@
 package com.rost;
 
+import java.io.IOException;
+
+import com.rost.services.GreetingServiceImpl;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8080)
+                .addService(GreetingServiceImpl.instance())
+                .build();
+
+        server.start();
+        System.out.println("Server has started! ;)");
+        server.awaitTermination();
     }
 }
